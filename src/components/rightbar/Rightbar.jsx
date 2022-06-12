b9573dc1a8705630a7ed804fd9be08219ae99f39
JRam2022 <jasonram778@gmail.com>
2022-06-12 13:11:17 -0700

    Create and Style rightbar and fix post css

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,46 @@
 import "./rightbar.css"
+import { Cake } from "@material-ui/icons"
+
 
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      Rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <Cake/>
+          <span className="birthdayText"><b>Jason</b> and 3 <b>other friend</b> have a birthday today</span>
+        </div>
+        <img className="rightbarAd" src="" alt="ADVERTISE" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jason Ram</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jason Ram</span>
+          </li><li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jason Ram</span>
+          </li><li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jason Ram</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
