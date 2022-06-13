aa94e316d044d847900b7a2e876b8d961e3cbb2e
Brandon Felty <brcfelty@gmail.com>
2022-06-13 14:57:06 -0600

    add api calls for login and login form handlers

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,21 @@
 import './login.css';
-
+import { loginCall } from '../../apiCalls';
+import {CircularProgress} from '@material-ui/core'
 import React, { useRef } from 'react'
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
+  const {user, isFetching, error, dispatch } = useContext(AuthContext); 
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(email.current.value)
+   loginCall({email:email.current.value, password:password.current.value}, dispatch)
   }
 
+  console.log(user)
   return (
     <div className='login'>
       <div className="loginWrapper">
@@ -30,9 +35,9 @@ export default function Login() {
               ref={password} 
               required 
               minLength="6"/>
-            <button className="loginButton">Login</button>
+            <button className="loginButton" type='submit' disabled={isFetching}>{isFetching ? <CircularProgress color='white' size="20px" /> : "Log In"}</button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegister">Create a New Account</button>
+            <button className="loginRegister">{isFetching ? <CircularProgress color='white' size="20px"/> : "Create a New Account"}</button>
           </form>
         </div>
       </div>
