e6d7d6bf01e1dc5d5bd390a534138a48c516183c
Brandon Felty <brcfelty@gmail.com>
2022-06-13 14:07:49 -0600

    adds form fields

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,16 @@
 import './login.css';
 
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(email.current.value)
+  }
+
   return (
     <div className='login'>
       <div className="loginWrapper">
@@ -13,13 +21,19 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input type="email" placeholder="Email" className="loginInput" ref={email}/>
+            <input 
+              type="password" 
+              placeholder="Password" 
+              className="loginInput" 
+              ref={password} 
+              required 
+              minLength="6"/>
             <button className="loginButton">Login</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegister">Create a New Account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
