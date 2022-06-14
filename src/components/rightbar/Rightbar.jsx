29e3f8207ae63d4c6558c0f1314f0a7e1140c6cd
Brandon Felty <brcfelty@gmail.com>
2022-06-14 14:55:03 -0600

    adds follow unfollow funcationality

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,18 +1,21 @@
 import "./rightbar.css";
-import { Cake } from "@material-ui/icons";
+import { Add, Cake } from "@material-ui/icons";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../../context/AuthContext";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Rightbar({user}) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  const [friends, setFriends] = useState([])
+  const [friends, setFriends] = useState([]);
+  const {user:currentUser, dispatch} = useContext(AuthContext);
+  const [followed, setFollowed] = useState(currentUser.following.includes(user?.id));
 
   useEffect(() => {
-    
     const getFriends = async() => {
       try {
         const friendList = await axios.get('/users/friends/' + user._id)
@@ -22,7 +25,22 @@ export default function Rightbar({user}) {
       }
     };
     getFriends();
-  },[user._id])
+  },[user]);
+
+  const handleClick = async() => {
+    try{
+      if(followed) {
+        await axios.put("/users/"+user._id+"/unfollow", {userId:currentUser._id});
+        dispatch({type: "UNFOLLOW", payload:user._id})
+      } else {
+        await axios.put("/users/"+user._id+"/follow", {userId:currentUser._id});
+        dispatch({type: "FOLLOW", payload:user._id})
+      }
+    }catch(err){
+      console.log(err);
+    }
+    setFollowed(!followed)
+  };
 
   const HomeRightBar = () => {
     return(
@@ -45,6 +63,12 @@ export default function Rightbar({user}) {
   const ProfileRightBar = () => {
     return(
       <>
+      {user.username !== currentUser.username && (
+        <button className="rightbarFollowButton" onClick={handleClick}>
+          {followed ? "Unfollow" : "Follow"}
+          {followed ? <RemoveIcon /> : <AddIcon/>}
+        </button>
+      )}
         <h4 className="rightbarTitle">User information</h4>
         <div className="rightBarInfo">
           <div className="rightbarInfoItem">
