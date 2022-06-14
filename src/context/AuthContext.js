57a1eb7dfc4f08270cc19818dc6014a2ac9b3d6c
Brandon Felty <brcfelty@gmail.com>
2022-06-14 14:11:07 -0600

    adds user to local storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: {  
-    _id: "62a4f6b18dd36c57e967fe14",
-    username: "jason",
-    email: "jason@gmail.com", 
-    profilePicture: "",  
-    coverPicture: "",  
-    followers: [],  
-    following: [],
-  },  
+  user: JSON.parse(localStorage.getItem("user")) || null,
     isFetching: false,
     error: false
 };
@@ -19,6 +11,10 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({children}) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(state.user))
+  },[state.user])
 
   return (
     <AuthContext.Provider
