089a5f94837f112735534217224795bc1d2b54c4
Brandon Felty <brcfelty@gmail.com>
2022-06-17 20:34:06 -0600

    fixes follow

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -13,7 +13,11 @@ export default function Rightbar({user}) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [friends, setFriends] = useState([]);
   const {user:currentUser, dispatch} = useContext(AuthContext);
-  const [followed, setFollowed] = useState(currentUser.following.includes(user?.id));
+  const [followed, setFollowed] = useState(null);
+
+  useEffect(() => {
+    setFollowed(currentUser.following.includes(user?._id))
+  }, [followed, user]);
 
   useEffect(() => {
     const getFriends = async() => {
@@ -38,10 +42,10 @@ export default function Rightbar({user}) {
         await axios.put("/users/"+user._id+"/follow", {userId:currentUser._id});
         dispatch({type: "FOLLOW", payload:user._id})
       }
+      setFollowed(!followed)
     }catch(err){
       console.log(err);
     }
-    setFollowed(!followed)
   };
 
   const HomeRightBar = () => {
